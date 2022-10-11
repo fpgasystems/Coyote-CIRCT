af811248956e89ca69ace6ce3e1aaa3e787bb412
Christian Ulmann <christianulmann@gmail.com>
2022-10-11 11:11:31 +0200

    change to custom buffering

--- scripts/export_std_loops.sh
+++ scripts/export_std_loops.sh
@@ -3,21 +3,33 @@
 # Add the binaries to the path
 export PATH="$PWD/circt-stream/build/bin:$PWD/circt-stream/circt/build/bin:$PATH" 
 
-hlstool hw/std/base/loop_sequence.mlir --dynamic-firrtl --buffering-strategy=all --dynamic-parallelism=none --ir | \
+stream-opt hw/std/base/loop_sequence.mlir --lower-std-to-handshake=disable-task-pipelining \
+  --canonicalize='top-down=true region-simplify=true' \
+  --handshake-materialize-forks-sinks --canonicalize \
+  --custom-buffer-insertion --handshake-insert-buffers=strategy=cycles \
+  --lower-handshake-to-firrtl | \
     firtool -format=mlir -o hw/std/base/gen_loop_sequence_none --split-verilog --lowering-options=disallowLocalVariables
 
 hlstool hw/std/driver.mlir --ir-input-level=1 --dynamic-firrtl --buffering-strategy=all --ir | \
   firtool -format=mlir -o hw/std/base/gen_loop_sequence_none/ --split-verilog --lowering-options=disallowLocalVariables
 
 
-hlstool hw/std/base/loop_sequence.mlir --dynamic-firrtl --buffering-strategy=all --dynamic-parallelism=locking --ir | \
-    firtool -format=mlir -o  hw/std/base/gen_loop_sequence_locking --split-verilog --lowering-options=disallowLocalVariables
+stream-opt hw/std/base/loop_sequence.mlir --lower-std-to-handshake=disable-task-pipelining \
+  --canonicalize='top-down=true region-simplify=true' --handshake-lock-functions \
+  --handshake-materialize-forks-sinks --canonicalize \
+  --custom-buffer-insertion --handshake-insert-buffers=strategy=cycles \
+  --lower-handshake-to-firrtl | \
+  firtool -format=mlir -o hw/std/base/gen_loop_sequence_locking/ --split-verilog --lowering-options=disallowLocalVariables
 
 hlstool hw/std/driver.mlir --ir-input-level=1 --dynamic-firrtl --buffering-strategy=all --ir | \
   firtool -format=mlir -o  hw/std/base/gen_loop_sequence_locking/ --split-verilog --lowering-options=disallowLocalVariables
 
 
-hlstool hw/std/base/loop_sequence.mlir --dynamic-firrtl --buffering-strategy=all --dynamic-parallelism=pipelining --ir | \
+stream-opt hw/std/base/loop_sequence.mlir --lower-std-to-handshake \
+  --canonicalize='top-down=true region-simplify=true' \
+  --handshake-materialize-forks-sinks --canonicalize \
+  --custom-buffer-insertion --handshake-insert-buffers=strategy=cycles \
+  --lower-handshake-to-firrtl | \
     firtool -format=mlir -o  hw/std/base/gen_loop_sequence_pipelining --split-verilog --lowering-options=disallowLocalVariables
 
 hlstool hw/std/driver.mlir --ir-input-level=1 --dynamic-firrtl --buffering-strategy=all --ir | \
--- scripts/export_std_nested.sh
+++ scripts/export_std_nested.sh
@@ -6,7 +6,8 @@ export PATH="$PWD/circt-stream/build/bin:$PWD/circt-stream/circt/build/bin:$PATH
 stream-opt hw/std/base/nested_diamond.mlir --lower-std-to-handshake=disable-task-pipelining \
   --canonicalize='top-down=true region-simplify=true' --handshake-lock-functions \
   --handshake-materialize-forks-sinks --canonicalize \
-  --handshake-insert-buffers=strategy=all --lower-handshake-to-firrtl | \
+  --custom-buffer-insertion --handshake-insert-buffers=strategy=cycles \
+  --lower-handshake-to-firrtl | \
   firtool -format=mlir -o hw/std/base/gen_nested_diamond_locking/ --split-verilog --lowering-options=disallowLocalVariables
 
 hlstool hw/std/driver.mlir --ir-input-level=1 --dynamic-firrtl --buffering-strategy=all --ir | \
