d6e241fa76f19837cd9e0972264f1d3ba08892f2
Christian Ulmann <christianulmann@gmail.com>
2022-10-11 16:08:52 +0200

    Change nested locking back to all buffering

--- scripts/export_std_nested.sh
+++ scripts/export_std_nested.sh
@@ -6,7 +6,7 @@ export PATH="$PWD/circt-stream/build/bin:$PWD/circt-stream/circt/build/bin:$PATH
 stream-opt hw/std/base/nested_diamond.mlir --lower-std-to-handshake=disable-task-pipelining \
   --canonicalize='top-down=true region-simplify=true' --handshake-lock-functions \
   --handshake-materialize-forks-sinks --canonicalize \
-  --custom-buffer-insertion --handshake-insert-buffers=strategy=cycles \
+  --handshake-insert-buffers=strategy=all \
   --lower-handshake-to-firrtl | \
   firtool -format=mlir -o hw/std/base/gen_nested_diamond_locking/ --split-verilog --lowering-options=disallowLocalVariables
 
